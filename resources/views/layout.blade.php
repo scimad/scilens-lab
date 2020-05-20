8525fabc922b29639dd2764db349111201a49de4
tat-tvam-asi <kvr24611@gmail.com>
2020-05-20 15:02:07 +0545

    Compile the assets (css and js) using NodePackageManager

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -19,18 +19,7 @@
 	<meta name="twitter:url" content="" />
 	<meta name="twitter:card" content="" />
 
-	<!-- Bootstrap  -->
-	<link rel="stylesheet" href="css/bootstrap.css">
-	<!-- Owl Carousel  -->
-	<link rel="stylesheet" href="css/owl.carousel.css">
-	<link rel="stylesheet" href="css/owl.theme.default.min.css">
-	<!-- Animate.css -->
-	<link rel="stylesheet" href="css/animate.css">
-	<!-- Font Awesome -->
-	<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.13/css/all.css" integrity="sha384-DNOHZ68U8hZfKXOrtjWvjxusGo9WQnrNx2sqG0tfsghAvtVlRW3tvkXWZh58N9jp" crossorigin="anonymous">
-
-	<!-- Theme style  -->
-	<link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="{{ asset('css/app.css' )}}">
 </head>
 
 <body>
@@ -102,5 +91,6 @@
 
         </div>
     </footer>
+    <script src="{{ asset('js/app.js' )}}">
 </body>
 </html>
