d3133ffbc0891dace9f8d5ab3517844de21f1f4f
tat-tvam-asi <kvr24611@gmail.com>
2020-06-12 17:43:52 +0545

    Collapsible Artisan tab; Delete @section('sidebar') from all blades and transfer it to layout.blade.php

--- resources/views/admin/main.blade.php
+++ resources/views/admin/main.blade.php
@@ -1,65 +1,5 @@
 @extends("admin.layout")
 
-@section('sidebar')
-    <ul class="nav">
-                <li class="nav-item active">
-                    <a href="/admin">
-                        <i class="la la-dashboard"></i>
-                        <p>Dashboard</p>
-                        <span class="badge badge-count">5</span>
-                    </a>
-                </li>
-                <li class="nav-item">
-                    <a href="/admin-components">
-                        <i class="la la-table"></i>
-                        <p>Components</p>
-                        <span class="badge badge-count">14</span>
-                    </a>
-                </li>
-                <li class="nav-item">
-                    <a href="/admin-artisan">
-                        <i class="la la-users"></i>
-                        <p>Artisan</p>
-                        <span class="badge badge-count">50</span>
-                    </a>
-                </li>
-                <li class="nav-item">
-                    <a href="tables.html">
-                        <i class="la la-th"></i>
-                        <p>Tables</p>
-                        <span class="badge badge-count">6</span>
-                    </a>
-                </li>
-                <li class="nav-item">
-                    <a href="notifications.html">
-                        <i class="la la-bell"></i>
-                        <p>Notifications</p>
-                        <span class="badge badge-success">3</span>
-                    </a>
-                </li>
-                <li class="nav-item">
-                    <a href="typography.html">
-                        <i class="la la-font"></i>
-                        <p>Typography</p>
-                        <span class="badge badge-danger">25</span>
-                    </a>
-                </li>
-                <li class="nav-item">
-                    <a href="icons.html">
-                        <i class="la la-fonticons"></i>
-                        <p>Icons</p>
-                    </a>
-                </li>
-                <li class="nav-item update-pro">
-                    <button  data-toggle="modal" data-target="#modalUpdate">
-                        <i class="la la-hand-pointer-o"></i>
-                        <p>Update To Pro</p>
-                    </button>
-                </li>
-            </ul>
-
-
-@endsection
 
 @section('content')
          <h4 class="page-title">Dashboard</h4>
@@ -140,82 +80,82 @@
                                 </div>
                             </div>
                         </div>
-                        <!--                            <div class="col-md-3">
-                                                        <div class="card card-stats">
-                                                            <div class="card-body ">
-                                                                <div class="row">
-                                                                    <div class="col-5">
-                                                                        <div class="icon-big text-center icon-warning">
-                                                                            <i class="la la-pie-chart text-warning"></i>
-                                                                        </div>
-                                                                    </div>
-                                                                    <div class="col-7 d-flex align-items-center">
-                                                                        <div class="numbers">
-                                                                            <p class="card-category">Number</p>
-                                                                            <h4 class="card-title">150GB</h4>
-                                                                        </div>
-                                                                    </div>
-                                                                </div>
-                                                            </div>
-                                                        </div>
-                                                    </div>
-                                                    <div class="col-md-3">
-                                                        <div class="card card-stats">
-                                                            <div class="card-body ">
-                                                                <div class="row">
-                                                                    <div class="col-5">
-                                                                        <div class="icon-big text-center">
-                                                                            <i class="la la-bar-chart text-success"></i>
-                                                                        </div>
-                                                                    </div>
-                                                                    <div class="col-7 d-flex align-items-center">
-                                                                        <div class="numbers">
-                                                                            <p class="card-category">Revenue</p>
-                                                                            <h4 class="card-title">$ 1,345</h4>
-                                                                        </div>
-                                                                    </div>
-                                                                </div>
-                                                            </div>
-                                                        </div>
-                                                    </div>
-                                                    <div class="col-md-3">
-                                                        <div class="card card-stats">
-                                                            <div class="card-body">
-                                                                <div class="row">
-                                                                    <div class="col-5">
-                                                                        <div class="icon-big text-center">
-                                                                            <i class="la la-times-circle-o text-danger"></i>
-                                                                        </div>
-                                                                    </div>
-                                                                    <div class="col-7 d-flex align-items-center">
-                                                                        <div class="numbers">
-                                                                            <p class="card-category">Errors</p>
-                                                                            <h4 class="card-title">23</h4>
-                                                                        </div>
-                                                                    </div>
-                                                                </div>
-                                                            </div>
-                                                        </div>
-                                                    </div>
-                                                    <div class="col-md-3">
-                                                        <div class="card card-stats">
-                                                            <div class="card-body">
-                                                                <div class="row">
-                                                                    <div class="col-5">
-                                                                        <div class="icon-big text-center">
-                                                                            <i class="la la-heart-o text-primary"></i>
-                                                                        </div>
-                                                                    </div>
-                                                                    <div class="col-7 d-flex align-items-center">
-                                                                        <div class="numbers">
-                                                                            <p class="card-category">Followers</p>
-                                                                            <h4 class="card-title">+45K</h4>
-                                                                        </div>
-                                                                    </div>
-                                                                </div>
-                                                            </div>
-                                                        </div>
-                                                    </div> -->
+                        <div class="col-md-3">
+                            <div class="card card-stats">
+                                <div class="card-body ">
+                                    <div class="row">
+                                        <div class="col-5">
+                                            <div class="icon-big text-center icon-warning">
+                                                <i class="la la-pie-chart text-warning"></i>
+                                            </div>
+                                        </div>
+                                        <div class="col-7 d-flex align-items-center">
+                                            <div class="numbers">
+                                                <p class="card-category">Number</p>
+                                                <h4 class="card-title">150GB</h4>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-md-3">
+                            <div class="card card-stats">
+                                <div class="card-body ">
+                                    <div class="row">
+                                        <div class="col-5">
+                                            <div class="icon-big text-center">
+                                                <i class="la la-bar-chart text-success"></i>
+                                            </div>
+                                        </div>
+                                        <div class="col-7 d-flex align-items-center">
+                                            <div class="numbers">
+                                                <p class="card-category">Revenue</p>
+                                                <h4 class="card-title">$ 1,345</h4>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-md-3">
+                            <div class="card card-stats">
+                                <div class="card-body">
+                                    <div class="row">
+                                        <div class="col-5">
+                                            <div class="icon-big text-center">
+                                                <i class="la la-times-circle-o text-danger"></i>
+                                            </div>
+                                        </div>
+                                        <div class="col-7 d-flex align-items-center">
+                                            <div class="numbers">
+                                                <p class="card-category">Errors</p>
+                                                <h4 class="card-title">23</h4>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-md-3">
+                            <div class="card card-stats">
+                                <div class="card-body">
+                                    <div class="row">
+                                        <div class="col-5">
+                                            <div class="icon-big text-center">
+                                                <i class="la la-heart-o text-primary"></i>
+                                            </div>
+                                        </div>
+                                        <div class="col-7 d-flex align-items-center">
+                                            <div class="numbers">
+                                                <p class="card-category">Followers</p>
+                                                <h4 class="card-title">+45K</h4>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
                     </div>
                     <div class="row">
                         <div class="col-md-3">
