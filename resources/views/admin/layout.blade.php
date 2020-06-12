b0258015a19dd1d49379658432ae88178d0f24f2
scimad <scimad2.71@gmail.com>
2020-06-12 18:31:08 +0545

    Add sidebar using partial

--- resources/views/admin/layout.blade.php
+++ resources/views/admin/layout.blade.php
@@ -167,75 +167,7 @@
                     </div>
                 </div>
             </div>
-    
-            <ul class="nav">
-                <li class="nav-item">
-                    <a href="/admin">
-                        <i class="la la-dashboard"></i>
-                        <p>Dashboard</p>
-                        <span class="badge badge-count">5</span>
-                    </a>
-                </li>
-                <li class="nav-item">
-                    <a href="/admin-components">
-                        <i class="la la-table"></i>
-                        <p>Components</p>
-                    </a>
-                </li>
-                <li class="nav-item">
-                    <a class="" data-toggle="collapse" href="#collapseArtisan" aria-expanded="true">
-                        <i class="la la-users"></i>
-                        <p>Artisan</p>
-                        <span class="caret"></span>
-                    </a>
-                    
-                    <div class="collapse in" id="collapseArtisan" aria-expanded="true">
-                        <ul class="nav">
-                            <li>
-                                <a href="/admin-artisan">
-                                    <span class="link-collapse">Recruit</span>
-                                </a>
-                            </li>
-                            <li>
-                                <a href="#view">
-                                    <span class="link-collapse">View All</span>
-                                </a>
-                            </li>
-                            <li>
-                                <a href="#fire">
-                                    <span class="link-collapse">Fire</span>
-                                </a>
-                            </li>
-                        </ul>
-                    </div>
-                </li>
-                <li class="nav-item">
-                    <a href="notifications.html">
-                        <i class="la la-bell"></i>
-                        <p>Notifications</p>
-                        <span class="badge badge-success">3</span>
-                    </a>
-                </li>
-                <li class="nav-item">
-                    <a href="typography.html">
-                        <i class="la la-font"></i>
-                        <p>Typography</p>
-                        <span class="badge badge-danger">25</span>
-                    </a>
-                </li>
-                <li class="nav-item">
-                    <a href="icons.html">
-                        <i class="la la-fonticons"></i>
-                        <p>Icons</p>
-                    </a>
-                </li>
-                <li class="nav-item update-pro">
-                    <button  data-toggle="modal" data-target="#modalUpdate">
-                        <i class="la la-hand-pointer-o"></i>
-                        <p>Update To Pro</p>
-                    </button>
-                </li>
-            </ul>
+            @include("admin.partials.sidebar")
         </div>
     </div>
 
