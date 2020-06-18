3eb59e478dc8c53ccc5dcf43020543bdcd03a1df
tat-tvam-asi <kvr24611@gmail.com>
2020-06-18 15:14:07 +0545

    Fix the views on the artisan submenus view all and fire

--- resources/views/admin/artisanfire.blade.php
+++ resources/views/admin/artisanfire.blade.php
@@ -1,6 +1,20 @@
 @extends('admin.layout.main')
 
+<style type="text/css">
+	.profieCard {
+	  box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2);
+	  border-radius: 10px;
+	  max-width: 300px;
+	  margin: auto;
+	  text-align: center;
+	  font-family: arial;
+	}
 
+	.profileTitle {
+	  color: grey;
+	  font-size: 18px;
+	}
+</style>
 @section('content')
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
 <script>
@@ -12,25 +26,37 @@ $(document).ready(function(){
 </script>
 <div class="row">
 
-	@if(count($staffs)>0)
+		@if(count($staffs)>0)
 		@foreach($staffs as $staff)
-			<div class="col">
+			<div class="col-lg-4">
 				<div style="margin:20px;">
 					<img class="image" src="/images/{{$staff->image}}" alt="Avatar" style="width:150px;border-radius: 50%; display: block; margin-left: auto; margin-right: auto;"><br>
-						<h3><strong>Name:</strong>{{$staff->name}}</h3>
-						<p><strong>Position:</strong>{{$staff->position}}</p>
-						<p><strong>Address:</strong>{{$staff->address}}</p>
+					<div class="profieCard bg-dark">
+						<h3>{{$staff->name}}</h3>
+						<p class="profileTitle">{{$staff->position}}</p>
 						<div style="margin: 10px 0;">
-							<strong>Facebook:</strong><a href="#">{{$staff->facebook}}</a> 
-							<strong>Instagram:</strong><a href="#">{{$staff->instagram}}</a>  
-							<strong>Twitter:</strong><a href="#">{{$staff->twitter}}</a>  
-							<strong>LinkedIn:</strong><a href="#">{{$staff->linkedin}}</a>  
+
+							@if (isset($staff->facebook))
+								<a href="#"><i class="fab fa-facebook"></i></a>
+							@endif
+							@if (isset($staff->instagram))
+								<a href="#"><i class="fab fa-instagram"></i></a>
+							@endif
+							@if (isset($staff->twitter))
+								<a href="#"><i class="fab fa-twitter"></i></a>
+							@endif
+							@if (isset($staff->linkedin))
+								<a href="#"><i class="fab fa-linkedin"></i></a>
+							@endif
+
 						</div>
-						<form action="{{ action('StaffsController@destroy' , $staff['id'])}}" method="post">
+							<form action="{{ action('StaffsController@destroy' , $staff['id'])}}" method="post">
 							@csrf
 							<input type="hidden" name="_method" value="DELETE"> 
 							<button class="btn btn-danger">Delete Staff</button>
 						</form>
+						<br>
+					</div>
 				</div>
 			</div>
 		@endforeach
